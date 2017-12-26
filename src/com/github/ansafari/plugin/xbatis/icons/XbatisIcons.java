a817d3a95a981f4c9a364995c1bb6b78e6d35626
xiongjinteng <xiongjinteng@gmail.com>
2017-12-26 15:33:18 +0800

    update:add icons,down the version

--- src/com/github/ansafari/plugin/xbatis/icons/XbatisIcons.java
+++ src/com/github/ansafari/plugin/xbatis/icons/XbatisIcons.java
@@ -9,4 +9,10 @@ public class XbatisIcons {
     public static final Icon NAVIGATE_TO_STATEMENT = IconLoader.getIcon("/icons/navigate_to_statement.png");
 
     public static final Icon NAVIGATE_TO_METHOD = IconLoader.getIcon("/icons/navigate_to_method.png");
+
+    public static final Icon NAVIGATE_TO_ALIAS = IconLoader.getIcon("/icons/alias.png");
+
+    public static final Icon NAVIGATE_TO_CONFIG = IconLoader.getIcon("/icons/config.png");
+
+    public static final Icon NAVIGATE_TO_MAPPER = IconLoader.getIcon("/icons/mapper.png");
 }
