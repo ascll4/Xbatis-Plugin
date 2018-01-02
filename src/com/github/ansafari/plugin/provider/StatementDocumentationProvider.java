e4755e4e792e5f31dcb326cfd9095f2a764da85b
xiongjinteng <xiongjinteng@gmail.com>
2018-01-02 20:49:33 +0800

    update:支持Mybatis的Quick Documentation 快速查看XML Statement

--- src/com/github/ansafari/plugin/provider/StatementDocumentationProvider.java
+++ src/com/github/ansafari/plugin/provider/StatementDocumentationProvider.java
@@ -1,6 +1,7 @@
 package com.github.ansafari.plugin.provider;
 
 import com.github.ansafari.plugin.ibatis.domain.sqlmap.SqlMapIdentifiableStatement;
+import com.github.ansafari.plugin.mybatis.domain.mapper.MapperIdentifiableStatement;
 import com.github.ansafari.plugin.service.DomFileElementsFinder;
 import com.intellij.lang.documentation.DocumentationProvider;
 import com.intellij.openapi.components.ServiceManager;
@@ -11,12 +12,17 @@ import com.intellij.psi.PsiManager;
 import com.intellij.psi.xml.XmlElement;
 import com.intellij.util.CommonProcessors;
 import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.regex.Pattern;
 
 /**
- * 字符串 预览 sql in xml
+ * control+J|F1 根据statement id查看XML元素源码.
+ * 如果不知道快捷键，可以通过Find Action（shift+command+a）搜索 Quick Documentation查看快捷方式
+ *
+ * @author xiongjinteng@raycloud.com
+ * @date 2018/1/2 20:36
  */
 public class StatementDocumentationProvider implements DocumentationProvider {
 
@@ -51,25 +57,45 @@ public class StatementDocumentationProvider implements DocumentationProvider {
                 id = parts[0];
             }
 
+            //先看Ibatis能不能找到，否则找Mybatis，最优是能够识别Dao是Ibatis还是Mybatis
+
             DomFileElementsFinder finder = ServiceManager.getService(originalElement.getProject(), DomFileElementsFinder.class);
-            CommonProcessors.FindFirstProcessor<SqlMapIdentifiableStatement> processor = new CommonProcessors.FindFirstProcessor<SqlMapIdentifiableStatement>();
+            CommonProcessors.FindFirstProcessor<SqlMapIdentifiableStatement> processor = new CommonProcessors.FindFirstProcessor<>();
             finder.processSqlMapStatements(namespace, id, processor);
             if (processor.isFound()) {
                 SqlMapIdentifiableStatement statement = processor.getFoundValue();
                 XmlElement xmlElement = statement != null ? statement.getXmlElement() : null;
                 if (xmlElement != null) {
-                    String text = "";
-                    if (StringUtils.isNotBlank(xmlElement.getText())) {
-                        text = xmlElement.getText().replaceAll("\n {4}", "\n");
-                    }
-                    return "<pre>" + StringUtil.escapeXml(text) + "</pre>";
+                    return getXmlElementCodeSource(xmlElement);
                 }
             }
 
+            CommonProcessors.FindFirstProcessor<MapperIdentifiableStatement> processorMapper = new CommonProcessors.FindFirstProcessor<>();
+            finder.processMapperStatements2(namespace, id, processorMapper);
+            if (processorMapper.isFound()) {
+                MapperIdentifiableStatement statement = processorMapper.getFoundValue();
+                XmlElement xmlElement = statement != null ? statement.getXmlElement() : null;
+                if (xmlElement != null) {
+                    return getXmlElementCodeSource(xmlElement);
+                }
+            }
             return null;
-
         }
         return null;
+    }
+
+    /**
+     * 获取XmlElement源代码
+     *
+     * @param xmlElement xmlElement
+     * @return String
+     */
+    private String getXmlElementCodeSource(@NotNull XmlElement xmlElement) {
+        String text = "";
+        if (StringUtils.isNotBlank(xmlElement.getText())) {
+            text = xmlElement.getText().replaceAll("\n {4}", "\n");
+        }
+        return "<pre>" + StringUtil.escapeXml(text) + "</pre>";
     }
 
     @Override
